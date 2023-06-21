b9b6ac3036ee42339be1204bced09db786c6becf
Will Chang <william.chaang@gmail.com>
2023-06-21 11:41:54 -0600

    Functions created

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2)
+};
+
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return function(integer){
+        return fare *integer 
+    }
+};
+
+function fareDoubler(fare){
+    return fare*2
+};
+
+function fareTripler(fare){
+    return fare*3
+};
+
+const selectDifferentDrivers = function (array, drivers){
+    return drivers(array)
+};
+
+/** Alternate, chunkier, solution:  
+function selectDifferentDrivers(arrayOfDrivers, f){
+    if (f === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(arrayOfDrivers)
+    } else if (f === returnLastTwoDrivers){
+        return returnLastTwoDrivers(arrayOfDrivers)
+    }
+};
+
+ */
+
